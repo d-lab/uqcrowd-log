7535d9a9314de5791c6bd3754e24e705643e9e6d
Andy LE <vuanh.le@uqconnect.edu.au>
2018-11-29 23:30:57 +1000

    remove try catch block

--- logger.py
+++ logger.py
@@ -1,32 +1,27 @@
-from __future__ import print_function
-import json, sys
 from elasticsearch import Elasticsearch
 from flask import Flask, request
 import redis
+import sys
 
-prefix = "/logger/v1.0/"
+api_prefix = "/logger/v1.0/"
+index_name = "uqcrowd-log"
 
 app = Flask(__name__)
 es = Elasticsearch()
 re = redis.Redis(host='localhost', port=6379, db=0)
-count = 0
 
-@app.route(prefix + "redis", methods=['POST'])
+
+@app.route(api_prefix + "redis", methods=['POST'])
 def insert():
-    try:
-        re.lpush('uqcrowd-log', request.data)
-        return "{success: True}"
-    except Exception as ex:
-        return ""
+    re.lpush(index_name, request.data)
+    return "True"
 
 
-@app.route(prefix + "index", methods=['POST'])
+@app.route(api_prefix + "index", methods=['POST'])
 def index():
-    global count
-    count += 1
-    print(request.data, file=sys.stdout)
-    es.index(index="uqcrowd-log", doc_type="uqcrowd-log", body=request.data)
-    return "{success: True}"
+    es.index(index=index_name, doc_type=index_name, body=request.data)
+    return "True"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=sys.argv[1], debug=False)
