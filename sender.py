366bbfb47f97c87361b8612b102f4734881e1556
Andy LE <vuanh.le@uqconnect.edu.au>
2018-12-17 20:19:06 +1000

    Refactor and add comments for sender.py

--- sender.py
+++ sender.py
@@ -1,97 +1,114 @@
 import json
 import time
 import redis
-import grequests
 import requests
 import threading
 from collections import deque
 from elasticsearch import Elasticsearch
-import datetime
+from datetime import datetime
 
-# url = "http://dke-uqcrowd-log.uqcloud.net/logger/v1.0/index"
-# url = 'http://localhost:5009/logger/redis'
-url = 'http://localhost:5000/logger/index'
+# logger_uri = "http://dke-uqcrowd-log.uqcloud.net/logger/v1.0/index"
+logger_uri = 'http://localhost:5000/logger/index'
 
 re = redis.Redis(host='localhost', port=6379, db=0)
 es = Elasticsearch()
 index_prefix = "uqcrowd-log"
 date_shift = 190
-count = 0
 
 
-def enqueue(q, m):
-    c = 0
-    with open('./data/mturklog.jl') as f:
+def load_messages_from_file(file_path, queue, max_count):
+    """ Load all the stored messages from file
+
+    Parameters:
+        file_path (string): The file that store all the message
+        queue (deque): The reference to the queue which will store all the messages
+        max_count (int): Maximum number of message to load
+    """
+    count = 0
+    with open(file_path) as f:
         for line in f:
-            c += 1
-            q.append(line.strip())
-            if c == m:
+            count += 1
+            queue.append(line.strip())
+            if count == max_count:
                 break
 
 
-def elastic(q):
-    while len(q) > 0:
-        m = json.loads(q.popleft())
-        new_date = datetime.datetime.strptime(m['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
-        new_date = new_date + datetime.timedelta(days=date_shift)
-        m['timestamp'] = new_date.isoformat();
-        m['server_time'] = new_date.isoformat();
+def send_to_elastic(queue):
+    """ Send all the message from queue to local elasticsearch server
 
-        index_name = index_prefix + "-" + new_date.strftime('%Y-%m-%d')
-        es.index(index=index_name, doc_type=index_prefix, body=json.dumps(m))
-        print(len(q))
-
-
-def redis(q):
-    while len(q) > 0:
-        m = q.popleft()
-        re.rpush(index_prefix, m)
-        print(len(q))
+    Parameters:
+        queue (deque): The reference to the queue which will store all the messages
+    """
+    while len(queue) > 0:
+        message = json.loads(queue.popleft())
+        index_name = index_prefix + "-" + datetime.now().strftime('%Y-%m-%d')
+        es.index(index=index_name, doc_type=index_prefix, body=json.dumps(message))
+        print(len(queue))
 
 
-def post(q):
-    while len(q) > 0:
-        m = q.popleft()
-        requests.post(url, data=m)
-        print(len(q))
+def send_to_redis(queue):
+    """ Send all the message from queue to local redis cache server
+
+    Parameters:
+        queue (deque): The reference to the queue which will store all the messages
+    """
+    while len(queue) > 0:
+        message = queue.popleft()
+        re.rpush(index_prefix, message)
+        print(len(queue))
 
 
-def async_post(q):
-    reqs = []
-    while len(q) > 0:
-        m = queue.popleft()
-        reqs.append(grequests.get(url, hooks={'response': handler}))
-    grequests.map(requests)
+def send_to_logger(queue, uri):
+    """ Send all the message from queue to local redis cache server
+
+    Parameters:
+        queue (deque): The reference to the queue which will store all the messages
+        uri (string: The uri of the logger server
+    """
+    while len(queue) > 0:
+        message = queue.popleft()
+        requests.post(uri, data=message)
+        print(len(queue))
 
 
-def handler(response, **kwargs):
-    global count
-    count += 1
-    print(count, response, kwargs)
-    response.close()
+def main():
+    queue = deque()
+    threads = []
+
+    # Number of message to send
+    number_of_message = 30000
+
+    # Number of concurrent process to send the messages
+    number_of_threads = 8
+
+    # Load all the messages from file to queue
+    load_messages_from_file("./data/mturklog.jl", queue, number_of_message)
+
+    start_time = time.time()
+
+    # Start multi-threading processes
+    for i in range(number_of_threads):
+
+        # Initialize the thread (Choose one of three options)
+        thread = threading.Thread(target=send_to_logger, args=(queue, logger_uri))
+        # thread = threading.Thread(target=send_to_elastic, args=(queue,))
+        # thread = threading.Thread(target=send_to_redis, args=(queue,))
+
+        # Start the thread and append it to the thread list for managing
+        thread.start()
+        threads.append(thread)
+
+    # Join all the thread together so all the thread will be terminated at same time when all the jobs a finished.
+    for i in range(number_of_threads):
+        threads[i].join()
+
+    # Calculate and print elapsed time
+    elapsed_time = time.time() - start_time
+    print(elapsed_time)
 
 
 if __name__ == "__main__":
-    queue = deque()
-    enqueue(queue, 30000)
-    concurrent = 1;
-    start_time = time.time()
-  
-    threads = []
-
-    for i in range(concurrent):
-        t = threading.Thread(target=elastic, args=(queue,))
-        # t = threading.Thread(target=redis, args=(queue,))
-        # t = threading.Thread(target=post, args=(queue,))
-        t.start()
-        threads.append(t)
-
-    for i in range(concurrent):
-        threads[i].join()
-
-    elapsed_time = time.time() - start_time
-
-    print(elapsed_time)
+    main()
 
 
 
